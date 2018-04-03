c6b09f1ed10245c979349a3bb6b0a742fb60e515
Sourav <esarve.srv@gmail.com>
2018-04-03 21:13:46 +0600

    redesigned dashboard

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -3,17 +3,11 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
-
 public class LoginController {
 
 
     @FXML
-    public AnchorPane loginPane;
-
-    @FXML
     public void openMainPanel(ActionEvent event) throws Exception {
         new Main().loadScene(Main.mainPanel);
-
     }
 }
