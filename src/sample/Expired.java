b18bd7d156259b5ec7196fc4359698d45f884cf6
Sourav <esarve.srv@gmail.com>
2018-04-09 20:06:10 +0600

    added expired items lol -_-

--- src/sample/Expired.java
+++ src/sample/Expired.java
@@ -1,13 +1,88 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
 
-public class Expired {
+public class Expired implements Initializable {
 
-    Main accessMain = new Main();
+    private Main accessMain = new Main();
+    Connection connection = DatabaseManager.DBconnect();
+    ObservableList<Item_details> data = FXCollections.observableArrayList();
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+
+
+    @FXML
+    private TableView<Item_details> itemlist;
+
+    @FXML
+    private TableColumn<?, ?> code;
+
+    @FXML
+    private TableColumn<?, ?> name;
+
+    @FXML
+    private TableColumn<?, ?> type;
+
+    @FXML
+    private TableColumn<?, ?> price;
+
+    @FXML
+    private TableColumn<?, ?> quantity;
+
+    @FXML
+    private TableColumn<?, ?> exp;
+
+    @FXML
+    private TableColumn<?, ?> supp;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        code.setCellValueFactory(new PropertyValueFactory<>("item_code"));
+        name.setCellValueFactory(new PropertyValueFactory<>("item_name"));
+        type.setCellValueFactory(new PropertyValueFactory<>("item_type"));
+        price.setCellValueFactory(new PropertyValueFactory<>("item_price"));
+        quantity.setCellValueFactory(new PropertyValueFactory<>("item_quantity"));
+        exp.setCellValueFactory(new PropertyValueFactory<>("exp_date"));
+        loadDataBase();
+    }
+
+    private void loadDataBase(){
+        try{
+            preparedStatement = connection.prepareStatement("SELECT  item.item_code, item.item_name, item.item_type, item_details.item_price, item_details.item_quantity, item_details.exp_date FROM `item_details`, `item` WHERE item.item_code=item_details.item_code and sysdate() > exp_date ORDER BY item_code");
+            resultSet=preparedStatement.executeQuery();
+            while (resultSet.next()){
+                data.add(new Item_details(
+                        resultSet.getString("item_code"),
+                        resultSet.getString("item_name"),
+                        resultSet.getString("item_type"),
+                        resultSet.getString("item_price"),
+                        resultSet.getString("item_quantity"),
+                        resultSet.getString("exp_date")
+                ));
+                itemlist.setItems(data);
+            }
+            preparedStatement.close();
+            resultSet.close();
+
+        }catch (Exception e){
+            System.err.println(e);
+        }
+    }
+
 
     @FXML
     void openDashboard(MouseEvent event) throws IOException {
